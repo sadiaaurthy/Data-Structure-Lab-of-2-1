5ecf4d5908519929e76f438634d280191ff15be5
sadiaaurthy <sadiaaurthy2005@gmail.com>
2025-12-23 22:07:26 +0600

    BST

--- DS_Lab06/230041114_T07L06_1B.cpp
+++ DS_Lab06/230041114_T07L06_1B.cpp
@@ -1,77 +1,63 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 struct Node {
     int data;
     Node* left;
     Node* right;
-    
-    Node(int val) {
-        data = val;
-        left = nullptr;
-        right = nullptr;
-    }
 };
+Node* CreateNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = nullptr;
+    temp->right = nullptr;
+    return temp;
+}
 class BST {
 private:
     Node* root;
-    int diameter;
-    Node* insert(Node* node, int val) {
+    int dm;
+    Node* insert(Node* node, int elm) {
         if (node == nullptr) {
-            return new Node(val);
+            return CreateNode(elm);
         }
-        
-        if (val < node->data) {
-            node->left = insert(node->left, val);
-        } else if (val > node->data) {
-            node->right = insert(node->right, val);
+        if (elm < node->data) {
+            node->left = insert(node->left, elm);
+        } else if (elm > node->data) {
+            node->right = insert(node->right, elm);
         }
-        
         return node;
     }
-
-    int calculateHeight(Node* node) {
-        if (node == nullptr) {
-            return 0;
-        }
-
-        int leftHeight = calculateHeight(node->left);
-        int rightHeight = calculateHeight(node->right);
-
-        diameter = max(diameter, leftHeight + rightHeight + 1);
-
-        return max(leftHeight, rightHeight) + 1;
+    int CalcHeight(Node* node) {
+        if (node == nullptr) return 0;
+        int LH = CalcHeight(node->left);
+        int RH = CalcHeight(node->right);
+        dm = max(dm, LH + RH + 1);
+        return max(LH, RH) + 1;
     }
-    
 public:
     BST() {
         root = nullptr;
-        diameter = 0;
+        dm = 0;
     }
-    
-    void insert(int val) {
-        root = insert(root, val);
+    void insert(int elm) {
+        root = insert(root, elm);
     }
-    
-    int getDiameter() {
-        diameter = 0;
-        calculateHeight(root);
-        return diameter;
+    int getDia() {
+        dm = 0;
+        CalcHeight(root);
+        return dm;
     }
 };
-
 int main() {
     int n;
     cin >> n;
-    
     BST bst;
-    
     for (int i = 0; i < n; i++) {
-        int val;
-        cin >> val;
-        bst.insert(val);
+        int elm;
+        cin >> elm;
+        bst.insert(elm);
     }
-    cout << bst.getDiameter() << endl;
+    cout << bst.getDia() << endl;
     return 0;
 }
