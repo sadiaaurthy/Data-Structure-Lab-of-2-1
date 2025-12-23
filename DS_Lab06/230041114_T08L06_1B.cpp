4197be1b72c04e30f78b606573100adf8dd417bd
sadiaaurthy <sadiaaurthy2005@gmail.com>
2025-12-23 22:20:22 +0600

    BST

--- DS_Lab06/230041114_T08L06_1B.cpp
+++ DS_Lab06/230041114_T08L06_1B.cpp
@@ -1,82 +1,71 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 struct Node {
     int data;
     Node* left;
     Node* right;
-    
-    Node(int val) {
-        data = val;
-        left = nullptr;
-        right = nullptr;
-    }
 };
-
+Node* CreateNode(int elm) {
+    Node* temp = new Node;
+    temp->data = elm;
+    temp->left = nullptr;
+    temp->right = nullptr;
+    return temp;
+}
 class BST {
 private:
     Node* root;
-
-    Node* insert(Node* node, int val) {
+    Node* insert(Node* node, int elm) {
         if (node == nullptr) {
-            return new Node(val);
+            return CreateNode(elm);
+        } 
+        if (elm < node->data) {
+            node->left = insert(node->left, elm);
+        } else if (elm > node->data) {
+            node->right = insert(node->right, elm);
         }
-        
-        if (val < node->data) {
-            node->left = insert(node->left, val);
-        } else if (val > node->data) {
-            node->right = insert(node->right, val);
-        }
-        
         return node;
     }
-
-    void findLeafNodes(Node* node, vector<int>& leaves) {
-        if (node == nullptr) {
-            return;
-        }
-
-        findLeafNodes(node->left, leaves);
-
+    void LeafNodesFinder(Node* node, vector<int>& leaves) {
+        if (node == nullptr) return;
+        LeafNodesFinder(node->left, leaves);
         if (node->left == nullptr && node->right == nullptr) {
             leaves.push_back(node->data);
         }
-        
-        findLeafNodes(node->right, leaves);
+        LeafNodesFinder(node->right, leaves);
     }
-    
 public:
     BST() {
         root = nullptr;
     }
-    
-    void insert(int val) {
-        root = insert(root, val);
+    void insert(int elm) {
+        root = insert(root, elm);
     }
-    
     vector<int> getLeafNodes() {
         vector<int> leaves;
-        findLeafNodes(root, leaves);
+        LeafNodesFinder(root, leaves);
         return leaves;
     }
 };
+
 
 int main() {
     int n;
     cin >> n;
     BST bst;
     for (int i = 0; i < n; i++) {
-        int val;
-        cin >> val;
-        bst.insert(val);
+        int elm;
+        cin >> elm;
+        bst.insert(elm);
     }
     vector<int> leafNodes = bst.getLeafNodes();
     cout << "Leaf nodes:" << endl;
     for (int i = 0; i < leafNodes.size(); i++) {
-        if (i > 0) cout << ", ";
+        if (i != 0) cout << ", ";
         cout << leafNodes[i];
     }
     cout << endl;
+
     return 0;
 }
